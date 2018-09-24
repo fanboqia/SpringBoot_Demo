ace279b697481566eec24c4c62d70eed0132c9a3
fanboqia <boqianfan@gmail.com>
2018-09-24 11:55:39 +0800

    SpringBoot Demo uploaded

---
+++ src/main/java/com/fbq/springboot/HelloController.java
@@ -0,0 +1,23 @@
+package com.fbq.springboot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+@Controller
+public class HelloController {
+
+    @Value("${name}")
+    private String name;
+
+    @RequestMapping("/hello")
+    public String hello(Model m){
+        m.addAttribute("now", DateFormat.getDateInstance().format(new Date()));
+        return "hello";
+    }
+}
